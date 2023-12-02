1f22bd66ded3c503ae5675723a4311dd5c8f7ebd
Astro <germiniasi.federico@gmail.com>
2023-12-02 09:40:50 +0100

    Aggiunto highScore

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cstdlib> // Per la dichiarazione di calloc
 #include <cstring> // Per la dichiarazione di memset
+#include <fstream>
+
 
 using namespace std;
 using namespace sf;
@@ -10,28 +12,90 @@ void initWindow(RenderWindow &window);
 
 #define FRAME_RATE 60
 #define AREAGIOCO 5
+#define BUFFERSTR 10000
 
-
+//roots
 const char rootFont[] = "./res/ClearSans.ttf"; 
+const char rootHighScore[] = "./res/data.txt";
+
+//colore del background
+const Vector2u dimFinestra(800, 600);
+
+//const color
+
 const Color QuadratoVuoto(205,193,180);
 //colore del background
 const Color BackGroundColor(187,173,160);
-const Vector2f dimFinestra(800, 600);
+
+const Color stage2(238,228,218);
+const Color stage4();
+const Color stage8();
+const Color stage16();
+const Color stage32();
+const Color stage64();
+const Color stage128();
+const Color stage256();
+const Color stage512();
+const Color stage1024();
+const Color stage2048();
+
+
+//const size
+const Vector2f dimQuadrato(100.0f,100.0f);
 
 class Game{
     private:
         int **areaGioco;
+        int highScore;
+        int currentScore;
         RectangleShape quadrato[AREAGIOCO][AREAGIOCO];
+        Text txtValori[AREAGIOCO][AREAGIOCO];
+        Font font;
+        Text txtHighScore;
+        Text titoloGioco;
 
     public:
-        Game(){
+        Game(RenderWindow &window){
             cout<<"Inizializzazione campo di gioco:\n";
+
+            
+            font.loadFromFile(rootFont);
+
             areaGioco = reinterpret_cast<int**>(calloc(AREAGIOCO, sizeof(int*)));
+            
             for (int i = 0; i < AREAGIOCO; ++i) {
                 areaGioco[i] = reinterpret_cast<int*>(calloc(AREAGIOCO, sizeof(int)));
             }
+            ifstream leggiFile(rootHighScore);
+            char highScoreStr[BUFFERSTR];
+            leggiFile >> highScoreStr;
+            leggiFile.close();
+            highScore=atoi(highScoreStr);
+
+            for(int i=0;i<AREAGIOCO;i++){
+                for(int j =0 ;j<AREAGIOCO;j++){
+                    txtValori[i][j].setString("0");
+                    txtValori[i][j].setCharacterSize(20);
+                    txtValori[i][j].setFont(font);
+                    txtValori[i][j].setStyle(Text::Bold);
+                    txtValori[i][j].setFillColor(Color(119,110,101));
+                    txtValori[i][j].setPosition(j * dimQuadrato.x+150+dimQuadrato.x/2, i * dimQuadrato.y+70+dimQuadrato.x/2);
+                    window.draw(txtValori[i][j]);
+                }
+            }
+            
+            
 
         }  
+        void setNewHighScore(int score){
+            highScore=score;
+            ofstream fileHighScore(rootHighScore);
+            char temp[BUFFERSTR];
+            sprintf(temp,"%d",score);
+            fileHighScore<<temp;
+            fileHighScore.close();
+
+        }
         bool setValore(int riga,int colonna,int valore){
             if(riga<0||riga>=AREAGIOCO||colonna<0||colonna>=AREAGIOCO){
                 return false;
@@ -41,12 +105,11 @@ class Game{
             }
         }
         void disegnaQuadrati(RenderWindow &window){//INIZIALIZZAZIONE DEI QUADRATI
-            Vector2f dimQuadrato(100.0f,100.0f);
+            
             quadrato[AREAGIOCO][AREAGIOCO];
-            Text titoloGioco;
+            
     
-            Font font;
-            font.loadFromFile(rootFont);
+            
             titoloGioco.setString("2048");
             titoloGioco.setCharacterSize(24);
             titoloGioco.setFont(font);
@@ -55,6 +118,20 @@ class Game{
             titoloGioco.setPosition(800/2-30,30);
             window.draw(titoloGioco);
             
+
+            
+            char highScoreStr[BUFFERSTR];
+            sprintf(highScoreStr,"Highscore: %d",highScore);
+            txtHighScore.setString(highScoreStr);
+            txtHighScore.setCharacterSize(24);
+            txtHighScore.setFont(font);
+            txtHighScore.setStyle(Text::Bold);
+            txtHighScore.setFillColor(Color(119,110,101));
+            txtHighScore.setPosition(30,30);
+            window.draw(txtHighScore);
+
+
+
             for(int i=0;i<AREAGIOCO;i++){
                 for(int j=0;j<AREAGIOCO;j++){
                     quadrato[i][j].setSize(dimQuadrato);
@@ -65,6 +142,54 @@ class Game{
                     window.draw(quadrato[i][j]);
                 }
             }
+            start();
+        }
+        void start(){
+            srand(time(NULL));
+            int x1,x2,y1,y2;
+            x1=rand()%AREAGIOCO;
+            x2=rand()%AREAGIOCO;
+            y1=rand()%AREAGIOCO;
+            y2=rand()%AREAGIOCO;
+
+            quadrato[x1][y1].setFillColor(stage2);
+            quadrato[x2][y2].setFillColor(stage2);
+
+            areaGioco[x1][y1]=2;
+            areaGioco[x2][y2]=2;
+        }
+        void update(RenderWindow &window){
+            window.draw(txtHighScore);
+            window.draw(titoloGioco);
+
+
+            char temp[BUFFERSTR];
+
+            for(int i=0;i<AREAGIOCO;i++){
+                for(int j=0;j<AREAGIOCO;j++){
+                    //aggiornamento valori gioco
+                    sprintf(temp,"%d",areaGioco[i][j]);
+                    txtValori[i][j].setString(temp);
+
+
+                    window.draw(quadrato[i][j]);
+                    if(areaGioco[i][j]!=0){
+                        window.draw(txtValori[i][j]);
+                    }
+                }
+            }
+        }
+        void toLeft(RenderWindow &window){
+            
+        }
+        void toUp(RenderWindow &window){
+            
+        }
+        void toRight(RenderWindow &window){
+            
+        }
+        void toDown(RenderWindow &window){
+            
         }
         
 
@@ -75,11 +200,11 @@ class Game{
 int main(){
     // create the window
     sf::RenderWindow window(sf::VideoMode(800, 600), "2048");
-    Game game;
+    Game game(window);
 
     
     initWindow(window);
-
+    game.disegnaQuadrati(window);
     // run the program as long as the window is open    
     while (window.isOpen())
     {
@@ -99,10 +224,28 @@ int main(){
 
         // clear the window with black color
         
+        if (sf::Keyboard::isKeyPressed(Keyboard::Left))
+        {
+            game.toLeft(window);
+        }
+        if (sf::Keyboard::isKeyPressed(Keyboard::Right))
+        {
+            game.toRight(window);
+        }
+        if (sf::Keyboard::isKeyPressed(Keyboard::Up))
+        {
+            game.toUp(window);
+        }
+        if (sf::Keyboard::isKeyPressed(Keyboard::Down))
+        {
+            game.toDown(window);
+        }
+        
+
 
         // draw everything here...
         // window.draw(...);
-        game.disegnaQuadrati(window);
+        game.update(window);
 
         // end the current frame
         window.display();
